3d28b4cf1133a1e63559de5b82852755948babfb
Ram Sah <mohan2036@gmail.com>
2022-12-01 10:29:02 -0500

    added files

---
+++ game3.js
@@ -0,0 +1,39 @@
+const displayChoices = document.getElementById('choices')
+const displayResult = document.querySelector('#result')
+const choices = ['rock', 'paper', 'scissors']
+
+const handleClick = (e) => {
+     getresult(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)])
+}
+choices.forEach(choice=> {
+    const button = document.createElement('button')
+    button.innerHTML = choice
+    displayChoices.appendChild(button)
+    button.addEventListener('click', handleClick)
+
+})
+
+const getresult = (userChoice, computerChoice)=>{
+    switch(userChoice+computerChoice){
+        case 'rockscissors':
+        case 'scissorspaper':
+        case 'paperrock':
+
+        displayResult.innerHTML=('you select: '+ userChoice + ' computer select :' + computerChoice +  '- you win')
+        break
+        case 'scissorsrock':
+            case 'paperscissors':
+            case 'rockpaper':
+
+            displayResult.innerHTML=('you select: '+ userChoice + ' computer select :' + computerChoice +  '- you lost')
+            break
+
+            case 'scissorsscissors':
+                case 'paperpaper':
+                case 'rockrock':
+
+                displayResult.innerHTML=('you select: '+ userChoice + ' computer select :' + computerChoice +  '- its a game')
+                break
+
+    }
+}
